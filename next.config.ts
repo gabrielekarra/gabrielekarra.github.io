f091940b25c4cd587dcac62fac2566d6ef210129
gabrielekarra <gabrielekarra@hotmail.it>
2024-11-28 16:18:34 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,24 @@
-import { NextConfig } from 'next';
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config, options) {
-    config.module?.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+	webpack(config, options) {
+		// Exclude SVGs from default image loader
+		const fileLoaderRule = config.module.rules.find(
+			(rule: { test: { test: (arg0: string) => any } }) =>
+				rule.test?.test?.(".svg"),
+		);
 
-    return config;
-  },
+		if (fileLoaderRule) {
+			fileLoaderRule.exclude = /\.svg$/;
+		}
+
+		config.module?.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
