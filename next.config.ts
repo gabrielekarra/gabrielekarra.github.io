3491cdfca52d795308aeb1f77932b174789a5733
Salvatore KARRA <salvatore.karra@elca.ch>
2024-11-29 16:15:55 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	webpack(config, options) {
-		config.module?.rules.push({
-			test: /\.svg$/,
-			use: ["@svgr/webpack"],
-		});
+  webpack(config, options) {
+    config.module?.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-		return config;
-	},
+    return config;
+  },
+  output: "export", // Ensure static export for GitHub Pages
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
 };
 
 export default nextConfig;
